a547f759912a64a90c09707d60692f4dc39616ca
llorens12 <daw2lpons@iesjoanramis.org>
2015-11-04 00:35:36 +0100

    Added control transition

--- js/conections.js
+++ js/conections.js
@@ -1,6 +1,13 @@
 function addVideoControls(){
+    document.getElementById("dummy-video").onclick = function(){
+        this.play();
+        document.getElementById("videoControls").style.opacity = '0';
 
+    };
 
 }
 
+function playPause(event){
 
+
+}
